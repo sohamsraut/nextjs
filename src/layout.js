528f61823cde8df4e0c56ac834e8cc13a18be5c9
sohamsraut <sohamr@uw.edu>
2023-08-09 19:09:58 +0400

    layout fixes

--- src/layout.js
+++ src/layout.js
@@ -4,7 +4,7 @@ import Footer from "@/components/Footer";
 export default function baseLayout(props) {
   return (
   <div className="flex flex-col w-screen h-screen justify-between ">
-    <Navbar/>
+    {/* <Navbar/> */}
     <div className="h-full w-full flex justify-center items-center">
       {props.children}
     </div>
