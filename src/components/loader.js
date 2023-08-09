9d7cdc7d8fbbd8009330a98ac17d7169d13dec71
sohamsraut <sohamr@uw.edu>
2023-08-09 14:30:20 +0400

    adds loader

---
+++ src/components/loader.js
@@ -0,0 +1,13 @@
+export default function loaderComponent() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen backdrop-blur shadow-xl">
+      <div className="flex flex-col justify-center items-center">
+        <svg id="triangle" width="100px" height="100px" viewbox="-3 -4 39 39">
+          <polygon className="stroke-[#4db5ff] " fill="transparent" stroke="#4db5ff" strokeWidth="2" points="16,0 32,32 0,32">
+          </polygon>
+        </svg>
+        <p className="text-3xl mt-3">Loading...</p>
+      </div>
+    </div>
+  );
+}
