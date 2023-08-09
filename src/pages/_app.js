ad4ba61cbbb53b6b12cac5444d88f7799a1db7c0
sohamsraut <sohamr@uw.edu>
2023-08-09 14:30:33 +0400

    app config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import BaseLayout from '@/layout';
+import { useState } from 'react';
+import LoaderComponent from '@/components/loader';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isLoading, setLoading] = useState(true);
+  if (isLoading) {
+    setTimeout(() => {setLoading(false);}, 3000);
+    return (<LoaderComponent />);
+  } else {
+    return (
+      <div className="flex flex-col w-screen flex-wrap h-screen">
+        <BaseLayout>
+          <Component {...pageProps} />
+        </BaseLayout>
+      </div>
+    );
+  }
 }
