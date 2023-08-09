525a722211ed4d4176922357760ebbabf62b8a22
sohamsraut <sohamr@uw.edu>
2023-08-09 14:30:10 +0400

    adds base layout

---
+++ src/layout.js
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function baseLayout(props) {
+  return (
+  <div className="flex flex-col w-screen h-screen justify-between ">
+    <Navbar/>
+    <div className="h-full w-full flex justify-center items-center">
+      {props.children}
+    </div>
+    <Footer />
+  </div>
+  );
+}
