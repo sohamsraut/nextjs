c4962bfb1ec9f0c6b834d23926c12932a233516e
sohamsraut <sohamr@uw.edu>
2023-08-09 19:09:46 +0400

    images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,19 @@
 export default function footer() {
   return (
     <footer
-    className="bg-[#0c0032]/[0.4] border-gray-200 relative w-full backdrop-opacity-10"
-  >
-      <div className="max-w-screen-3xl flex flex-col items-center justify-between ml-2">
-        <div>Soham Raut</div>
-        <ul>Tabs here</ul>
-        <ul>links here</ul>
-        <div>Thank you! &copy; Soham Raut, 2023</div>
+    className="bg-[#4db5ff] relative w-full backdrop-opacity-10 text-[#1b1b2b]" style={{backgroundImage: "linear-gradient(#4db5ff, #4da5ff)"}}
+    >
+      <div className="max-w-screen-3xl flex flex-col items-center justify-between">
+        <div className="text-4xl mt-16 font-bold heading">Soham Raut</div>
+        <ul className="my-7">
+          Tabs here
+        </ul>
+        <ul className="flex space-x-6 mb-16">
+          <li><a href="mailto:sohamsraut97@gmail.com" target="_blank"><img src="./envelope-solid.svg" className="w-[40px] h-[40px]"/></a></li>
+          <li><a href="https://github.com/sohamsraut" target="_blank"><img src="./square-github.svg" className="w-[40px] h-[40px]"/></a></li>
+          <li><a href="https://linkedin.com/in/sohamsraut" target="_blank"><img src="./linkedin.svg" className="w-[40px] h-[40px]"/></a></li>
+        </ul>
+        <div className="text-sm mb-16">Thank you! &copy; Soham Raut, 2023</div>
       </div>
     </footer>
   );
