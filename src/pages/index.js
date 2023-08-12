0cdcc0813a04d6e6c72c69d279df6fae06837798
sohamsraut <sohamr@uw.edu>
2023-08-12 17:59:49 +0400

    simplifies code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,28 @@
-import LargeDeviceView from "@/largeDeviceView";
-import { useState, useEffect } from "react";
-import LoaderComponent from "@/components/loader";
+import HomePageComponent from "@/components/homePage";
+import Head from "next/head";
+import AboutSection from "@/components/about";
+import Footer from "@/components/Footer";
+import SkillSection from "@/components/skills";
+import EducationSection from "@/components/education";
+import ExperienceSection from "@/components/experience";
+import ProjectSection from "@/components/projects";
+import ContactForm from "@/components/contactForm";
 
 // #1b1b1b and #ffb400
 export default function home() {
-    const [windowSize, setWindowSize] = useState({
-      width: undefined,
-      height: undefined
-    });
-
-    const [loaded, setLoaded] = useState(false);
-
-    useEffect(() => {
-      function handleResize() {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight
-        });
-      }
-
-      window.addEventListener("resize", handleResize);
-
-      // Call handler right away so state gets updated with initial window size
-      handleResize();
-
-      // Don't forget to remove event listener on cleanup
-      return () => window.removeEventListener("resize", handleResize);
-    }, []);
-  console.log(windowSize); // < 768 => display change
-  // if (windowSize.width === undefined || !loaded) {
-  //   setTimeout(() => {setLoaded(true)}, 2000);
-  //   return (<LoaderComponent />);
-  // } else if (windowSize.width > 750) {
-      return (<LargeDeviceView />)
-  // } else {
-  //   return <LoaderComponent />;
-  // }
+  return (
+    <div className="w-full h-full flex flex-col">
+      <Head>
+        <title>Soham Raut | Home</title>
+      </Head>
+      <HomePageComponent />
+      <AboutSection />
+      <SkillSection />
+      <EducationSection />
+      <ExperienceSection />
+      <ProjectSection />
+      <ContactForm />
+      <Footer />
+    </div>
+  )
 }
